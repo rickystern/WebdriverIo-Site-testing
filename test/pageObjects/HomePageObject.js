19d848d862863d1a3ad1b3e28dae7e1adb306369
Sterneus <rohanstern9@outlook.com>
2019-08-14 16:40:10 -0500

    clean up

--- test/pageObjects/HomePageObject.js
+++ test/pageObjects/HomePageObject.js
@@ -1,14 +1,13 @@
 
 
 let basePageClass = class Page {
-
-    open(path) {
-        browser.url('/');
-    }
     get headerItems() { return $$('.slidingNav > ul > li> a'); }
     get searchBar() { return $('.algolia-autocomplete > input'); }
     get displayBar() { return $('#algolia-autocomplete-listbox-0 > div'); }
-    
+
+     open(path) {
+        browser.url('/');
+    }
 }
 
 module.exports.basePageClass = new basePageClass();
